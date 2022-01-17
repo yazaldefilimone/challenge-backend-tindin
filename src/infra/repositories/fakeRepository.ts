58ba789396c861b297de7869fe84a0c23b36469e
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-17 15:50:31 +0200

    crie o fake repository local

---
+++ src/infra/repositories/fakeRepository.ts
@@ -0,0 +1,34 @@
+import { randomUUID } from "crypto";
+
+import { IUserRepository } from "@/data/contracts";
+import { userDTO } from '@/data/dtos'
+
+export class FakeUserRepository implements IUserRepository{
+  private readonly database:userDTO[]
+  constructor(){
+    this.database = [];
+  }
+
+  async find (): Promise<userDTO[]>{
+    return this.database;
+  }
+
+  async findByEmail (email: string) : Promise<userDTO | null>{
+    const userOrNull = this.database.find(user => user.email === email);
+
+    return userOrNull
+  }
+
+  async create({ name, email, password }: userDTO) : Promise<userDTO>{
+    let user:userDTO = {
+      _id: randomUUID(),
+      name,
+      email,
+      password
+    }
+    
+    this.database.push(user);
+
+    return user;
+  }
+}
