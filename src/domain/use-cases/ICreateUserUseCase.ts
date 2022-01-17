b6704a47128ab7b334862bb17eda5ed2de69457b
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-17 15:30:24 +0200

    retornando o either

--- src/domain/use-cases/ICreateUserUseCase.ts
+++ src/domain/use-cases/ICreateUserUseCase.ts
@@ -1,5 +1,7 @@
 import { User } from '@/domain/entities';
+import { AlreadyExists, InvalideParamsError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
 
 export interface ICreateUserUseCase{
-  execute: ({ name, email, password } :User) => Promise<User>
+  execute: ({ name, email, password } :User) => Promise<Either<AlreadyExists| InvalideParamsError, User>>
 }
