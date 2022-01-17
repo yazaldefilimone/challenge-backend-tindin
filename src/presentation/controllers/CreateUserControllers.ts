ab478ebf03dd838c130cfdab06000a45b8fb4d84
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-17 16:54:51 +0200

    crie o controller de usuario

---
+++ src/presentation/controllers/CreateUserControllers.ts
@@ -0,0 +1,22 @@
+import { ICreateUserUseCase } from "../../domain/use-cases"
+import { IController } from "../../presentation/contracts";
+import { httpResponse } from "../../presentation/contracts/http"
+import { badRequest, create } from '../../presentation/contracts/http'
+
+import { Request } from "express";
+
+export class CreateUserController implements IController{
+  constructor(private readonly createUserUseCase: ICreateUserUseCase){}
+
+  async handle(request: Request): Promise<httpResponse>{
+    const { name, email, password }  = request.body;
+    const userOrError = await this.createUserUseCase.execute({ name, email, password });
+
+    if(userOrError.isLeft()){
+      return badRequest(userOrError.value);
+    }
+
+    return create(userOrError.value);
+    
+  }
+}
