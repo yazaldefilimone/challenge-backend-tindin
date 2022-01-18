a9f011da4b80fb1ad4ef725ae85c028201eb3a55
yazalde filimone pinto <yazaldefilimon@gmail.com>
2022-01-18 06:02:42 +0200

    modei a interface de Number para number

--- src/data/contracts/IUserRepository.ts
+++ src/data/contracts/IUserRepository.ts
@@ -2,6 +2,6 @@ import { userDTO } from "@/data/dtos";
 
 export interface IUserRepository {
   find: () => Promise<userDTO[]>;
-  findByEmail: (email:string) => Promise<userDTO | null>;
+  findByEmail: (email:string) => Promise<userDTO | undefined>;
   create: ({ name, email, password }: userDTO) => Promise<userDTO>;
 }
